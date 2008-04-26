eb7852aad17ec7ac35c71ac9e0044dc920c10ce7
John Wiegley <johnw@newartisans.com>
2008-04-25 23:14:41 -0400

    Added a build.sh script for cold bootstrapping.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PPC_HOST=$1
+
+# Bootstrap a Ready Lisp disk image from complete scratch.  Just make sure all
+# relevant version numbers and URLs have been updated and pushed to the Github
+# repository first.
+
+cd /tmp
+
+rm -fr ready-lisp
+
+ssh $PPC_HOST rm -fr /tmp/ready-lisp
+
+git clone git://github.com/jwiegley/ready-lisp.git && \
+    cd ready-lisp && \
+    time make PPC_HOST=$PPC_HOST
+
+# build.sh ends here
